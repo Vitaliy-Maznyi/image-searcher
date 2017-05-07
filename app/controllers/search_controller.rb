f6d3c693104f0e196b4f2aaa102172b9b7b7f073
VitaliyMaznyi <vitaliymaznuy@gmail.com>
2017-05-07 21:00:05 +0300

    added pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,12 @@
 class SearchController < ApplicationController
   def index
-    if params[:search]
-      query = Search.search_by(query: params[:search][:query])
+    if params[:search] || params[:page]
+      query = Search.search_by(query: params[:search][:query], page: params[:page])
       respond_to do |format|
-        format.html { @photos = JSON.parse(query['results'].to_json, object_class: OpenStruct) }
+        format.html do
+          @photos = JSON.parse(query.to_json, object_class: OpenStruct)
+          @photos.search = params[:search][:query] # save search query throw pagination
+        end
         format.json { render json: query }
       end
     else
--- app/services/search.rb
+++ app/services/search.rb
@@ -2,11 +2,12 @@ class Search
   include HTTParty
   base_uri 'api.unsplash.com'
 
-  def self.search_by(query = '')
+  def self.search_by(query = '', page = '1')
     get('/search/photos', headers: {
       'Authorization' => 'Client-ID ef9d6f296a30daeebb560a1c001b2d72d43bb04b9ecda9a3789f8d1aa0b6fce0'
     },
-                          query: query)
+                          query: query,
+                          page: page)
   end
 
   def self.show_image(image = '')
